cbd52d20ee8b92e0ef672be0b59e54880d8b1c31
nom0531 <norrrrrra0504@gmail.com>
2024-10-30 09:41:23 +0900

    オプションの処理を実装。

--- Assets/Script/Option/ScreenSwitch_Option.cs
+++ Assets/Script/Option/ScreenSwitch_Option.cs
@@ -1,39 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
+/// <summary>
+/// 選択中のコマンド。
+/// </summary>
+public enum OptionState
+{
+    enBGMSound,
+    enSESound,
+    enCamera,
+    enReset,
+    enBGMParamator,     // BGMのパラメータ。
+    enSEParamator,      // SEのパラメータ。
+    enCameraParamator,  // カメラのパラメータ。
+}
 
 public class ScreenSwitch_Option : MonoBehaviour
 {
     [SerializeField, Header("SE"), Tooltip("決定音")]
     SE SE_Determination;
     [SerializeField, Tooltip("キャンセル音")]
-    SE SE_Chancel;
-    [SerializeField, Tooltip("セレクト音")]
-    SE SE_Select;
-
-    /// <summary>
-    /// 選択中のコマンド。
-    /// </summary>
-    private enum ComandState
-    {
-        enBGMSound,
-        enSESound,
-        enCamera,
-        enReset
-    }
+    SE SE_Cancel;
+    [SerializeField, Tooltip("カーソル移動音")]
+    SE SE_CursorMove;
+    [SerializeField, Tooltip("エラー音")]
+    SE SE_Error;
 
     private SaveDataManager m_saveDataManager;
-    private SoundManager m_soundManager;
+    private Cursor m_cursor;
     private SceneChange m_sceneChange;
-    private ComandState m_comandState = ComandState.enBGMSound;
+    private SetParamator m_setParamator;
+    private Gamepad m_gamepad;
+    private OptionState m_comandState = OptionState.enBGMSound;
     private bool m_isPush = false;    // ボタンを押したならture。
 
     // Start is called before the first frame update
     private void Start()
     {
         m_saveDataManager = GameManager.Instance.SaveDataManager;
-        m_soundManager = GameManager.Instance.SoundManager;
+        m_cursor = GameObject.FindGameObjectWithTag("Cursor").GetComponent<Cursor>();
         m_sceneChange = GetComponent<SceneChange>();
+        m_setParamator = GetComponent<SetParamator>();
     }
 
     // Update is called once per frame
@@ -44,7 +53,56 @@ public class ScreenSwitch_Option : MonoBehaviour
             return;
         }
         SelectCommand();
-        SceneChange();
+        ButtonDown();
+    }
+
+    /// <summary>
+    /// ボタン処理。
+    /// </summary>
+    private void ButtonDown()
+    {
+        // Aボタンを押したとき。
+        if (Input.GetKeyDown("joystick button 0") || Input.GetKeyDown(KeyCode.J))
+        {
+            SceneChange();
+            ChangeState();
+            SE_Cancel.PlaySE();
+            m_saveDataManager.Save();
+        }
+        // Bボタンを押したとき。
+        if (Input.GetKeyDown("joystick button 1") || Input.GetKeyDown(KeyCode.K))
+        {
+            ButtonPush();
+            SE_Determination.PlaySE();
+        }
+    }
+
+    /// <summary>
+    /// シーンを切り替える処理。
+    /// </summary>
+    private void SceneChange()
+    {
+        if(m_comandState > OptionState.enReset)
+        {
+            return;
+        }
+        // シーンを切り替える。
+        m_sceneChange.CreateFadeCanvas();
+        m_isPush = true;
+    }
+
+    /// <summary>
+    /// ステートを変更する。
+    /// </summary>
+    private void ChangeState()
+    {
+        if(m_comandState < OptionState.enBGMParamator)
+        {
+            return;
+        }
+        // 選択中のコマンドを変更する。
+        m_comandState -= 4;
+        m_cursor.Move((int)m_comandState);
     }
 
     /// <summary>
@@ -52,43 +110,81 @@ public class ScreenSwitch_Option : MonoBehaviour
     /// </summary>
     private void SelectCommand()
     {
-        // ↑キーを押したとき。
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            m_comandState--;
-            // 補正。
-            if (m_comandState < ComandState.enBGMSound)
-            {
-                m_comandState = ComandState.enReset;
-            }
-            SE_Select.PlaySE();
-        }
-        // ↓キーを押したとき。
-        if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            m_comandState++;
-            // 補正。
-            if (m_comandState > ComandState.enReset)
-            {
-                m_comandState = ComandState.enBGMSound;
-            }
-            SE_Select.PlaySE();
-        }
-        Debug.Log(m_comandState);
+        // ゲームパッドを取得。
+        m_gamepad = Gamepad.current;
+
+        CursorMove();
+        m_setParamator.Set(m_comandState, m_gamepad);
     }
 
     /// <summary>
-    /// 遷移処理。
+    /// カーソル移動。
     /// </summary>
-    private void SceneChange()
+    private void CursorMove()
     {
-        // Bボタンを押したとき。
-        if (Input.GetKeyDown("joystick button 1") || Input.GetKeyDown(KeyCode.K))
+        if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            m_sceneChange.CreateFadeCanvas();
-            SE_Chancel.PlaySE();
-            m_isPush = true;
+            PushUp();
         }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            PushDown();
+        }
+
+        // ゲームパッドが接続されていない場合。
+        if(m_gamepad == null)
+        {
+            return;
+        }
+
+        if (m_gamepad.dpad.up.wasPressedThisFrame)
+        {
+            PushUp();
+        }
+        if (m_gamepad.dpad.down.wasPressedThisFrame)
+        {
+            PushDown();
+        }
+    }
+
+    /// <summary>
+    /// ↑キーを押したときの処理。
+    /// </summary>
+    private void PushUp()
+    {
+        if (m_comandState > OptionState.enReset)
+        {
+            SE_Error.PlaySE();
+            return;
+        }
+        m_comandState--;
+        // 補正。
+        if (m_comandState < OptionState.enBGMSound)
+        {
+            m_comandState = OptionState.enReset;
+        }
+        m_cursor.Move((int)m_comandState);
+        SE_CursorMove.PlaySE();
+    }
+
+    /// <summary>
+    /// ↓キーを押したときの処理。
+    /// </summary>
+    private void PushDown()
+    {
+        if (m_comandState > OptionState.enReset)
+        {
+            SE_Error.PlaySE();
+            return;
+        }
+        m_comandState++;
+        // 補正。
+        if (m_comandState > OptionState.enReset)
+        {
+            m_comandState = OptionState.enBGMSound;
+        }
+        m_cursor.Move((int)m_comandState);
+        SE_CursorMove.PlaySE();
     }
 
     /// <summary>
@@ -99,27 +195,19 @@ public class ScreenSwitch_Option : MonoBehaviour
         // ステートに応じて処理を変更。
         switch (m_comandState)
         {
-            case ComandState.enBGMSound:
+            case OptionState.enBGMSound:
+                m_comandState = OptionState.enBGMParamator;
                 break;
-            case ComandState.enSESound:
+            case OptionState.enSESound:
+                m_comandState = OptionState.enSEParamator;
                 break;
-            case ComandState.enCamera:
+            case OptionState.enCamera:
+                m_comandState = OptionState.enCameraParamator;
                 break;
-            case ComandState.enReset:
-                ResetStatus();
+            case OptionState.enReset:
+                m_setParamator.ResetStatus();
                 break;
         }
-        SE_Determination.PlaySE();
-    }
-
-    /// <summary>
-    /// データを初期化する。
-    /// </summary>
-    private void ResetStatus()
-    {
-        // 音量をデフォルトに戻す。
-
-        // カメラ回転をデフォルトに戻す。
-        Debug.Log("初期値に戻す");
+        m_cursor.Move((int)m_comandState);
     }
 }
---
+++ Assets/Script/Option/SetParamator.cs
@@ -0,0 +1,351 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+using UnityEngine.UI;
+
+public class SetParamator : MonoBehaviour
+{
+    [SerializeField, Header("カメラ"),Tooltip("テキスト")]
+    private GameObject CameraText;
+    [SerializeField, Tooltip("カーソル")]
+    private GameObject[] CameraCursor;
+    [SerializeField, Header("サウンド"), Tooltip("カーソル")]
+    private GameObject[] SoundCursor;
+    [SerializeField, Tooltip("テキスト")]
+    private GameObject[] SoundText;
+    [SerializeField, Tooltip("限度値")]
+    private float PositionX_Min, PositionX_Max;
+    [SerializeField, Tooltip("段階")]
+    private int SoundStage = 10;
+    [SerializeField, Header("SE"), Tooltip("カーソル移動音")]
+    private SE SE_CursorMove;
+    [SerializeField, Tooltip("エラー音")]
+    SE SE_Error;
+
+    /// <summary>
+    /// サウンドデータ。
+    /// </summary>
+    struct SoundData
+    {
+        public RectTransform rectTransform; // 座標。
+        public int soundStage;              // 現在の音量。
+    }
+
+    /// <summary>
+    /// サウンドステート。
+    /// </summary>
+    private enum SoundState
+    {
+        enBGM,
+        enSE,
+        enNum
+    }
+
+    private SaveDataManager m_saveDataManager;
+    private SoundManager m_soundManager;
+    private Gamepad m_gamepad;
+    private BGM m_bgm;
+    private SoundData[] m_soundDatas = new SoundData[(int)SoundState.enNum];
+    private OptionState m_comandState = OptionState.enBGMSound;
+    private float m_moveLength = 0.0f;                          // 一度のカーソルの移動量。
+
+
+    private void Start()
+    {
+        m_saveDataManager = GameManager.Instance.SaveDataManager;
+        m_soundManager = GameManager.Instance.SoundManager;
+        m_bgm = GameObject.FindGameObjectWithTag("BGM").GetComponent<BGM>();
+        m_soundDatas[(int)SoundState.enBGM].rectTransform = SoundCursor[(int)SoundState.enBGM].GetComponent<RectTransform>();
+        m_soundDatas[(int)SoundState.enSE].rectTransform = SoundCursor[(int)SoundState.enSE].GetComponent<RectTransform>();
+
+        // 移動量を計算。
+        m_moveLength = (PositionX_Min - PositionX_Max) / SoundStage;
+
+        Init();
+    }
+
+    /// <summary>
+    /// 初期化処理。
+    /// </summary>
+    private void Init()
+    {
+        ChangeCameraRotation(m_saveDataManager.SaveData.saveData.CameraStete);
+        InitSoundVolume();
+    }
+
+    /// <summary>
+    /// 音量の初期化。
+    /// </summary>
+    private void InitSoundVolume()
+    {
+        // ボリュームを計算。
+        var bgm = m_saveDataManager.SaveData.saveData.BGMVolume * SoundStage;
+        var se = m_saveDataManager.SaveData.saveData.SEVolume * SoundStage;
+        // 番号。
+        int bgmNumber = (int)SoundState.enBGM;
+        int seNumber = (int)SoundState.enSE;
+
+        // カーソル位置を更新。
+        CursorMove(bgm, bgmNumber);
+        CursorMove(se, seNumber);
+        // 現在の段階。（0～SoundStageまで）
+        m_soundDatas[bgmNumber].soundStage = (int)bgm;
+        m_soundDatas[seNumber].soundStage = (int)se;
+        // テキストを更新。
+        SoundText[bgmNumber].GetComponent<TextMeshProUGUI>().text = m_soundDatas[bgmNumber].soundStage.ToString();
+        SoundText[seNumber].GetComponent<TextMeshProUGUI>().text = m_soundDatas[seNumber].soundStage.ToString();
+        // 音量を適用する。
+        m_bgm.ResetVolume();
+    }
+
+    /// <summary>
+    /// カーソルの位置を決定する。
+    /// </summary>
+    /// <param name="soundStage">音量の段階。</param>
+    private void CursorMove(float soundStage, int number)
+    {
+        // 現在の音量と更新後の音量を比較。
+        var diff = (int)soundStage - m_soundDatas[number].soundStage;
+        // カーソルの移動量を計算。
+        MoveX(m_soundDatas[number].rectTransform, diff * -m_moveLength);
+    }
+
+    /// <summary>
+    /// パラメータを設定する処理。
+    /// </summary>
+    public void Set(OptionState comandState, Gamepad gamepad)
+    {
+        m_comandState = comandState;
+        m_gamepad = gamepad;
+
+        SetBGMParamator();
+        SetSEParamator();
+        SetCameraParamator();
+    }
+
+    /// <summary>
+    /// BGMのパラメータを設定する処理。
+    /// </summary>
+    private void SetBGMParamator()
+    {
+        if (m_comandState != OptionState.enBGMParamator)
+        {
+            return;
+        }
+        SetVolume(SoundState.enBGM);
+    }
+
+    /// <summary>
+    /// SEのパラメータを設定する処理。
+    /// </summary>
+    private void SetSEParamator()
+    {
+        if (m_comandState != OptionState.enSEParamator)
+        {
+            return;
+        }
+        SetVolume(SoundState.enSE);
+    }
+
+    /// <summary>
+    /// 音量を設定。
+    /// </summary>
+    /// <param name="rectTransform">対象オブジェクトの座標。</param>
+    private void SetVolume(SoundState soundState)
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            VolumeUp((int)soundState);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            VolumeDown((int)soundState);
+        }
+
+        // ゲームパッドが接続されていない場合。
+        if(m_gamepad == null)
+        {
+            return;
+        }
+
+        if (m_gamepad.dpad.right.wasPressedThisFrame)
+        {
+            VolumeUp((int)soundState);
+        }
+        if (m_gamepad.dpad.left.wasPressedThisFrame)
+        {
+            VolumeDown((int)soundState);
+        }
+    }
+
+    /// <summary>
+    /// 音量を上げる処理。
+    /// </summary>
+    private void VolumeUp(int number)
+    {
+        m_soundDatas[number].soundStage++;
+        if (m_soundDatas[number].soundStage > SoundStage)
+        {
+            m_soundDatas[number].soundStage = SoundStage;
+            SE_Error.PlaySE();
+            return;
+        }        
+
+        MoveX(m_soundDatas[number].rectTransform, -m_moveLength);
+        SoundText[number].GetComponent<TextMeshProUGUI>().text = m_soundDatas[number].soundStage.ToString();
+
+        if (m_comandState == OptionState.enSEParamator)
+        {
+            m_saveDataManager.SaveData.saveData.SEVolume = (float)m_soundDatas[number].soundStage / SoundStage;
+        }
+        if (m_comandState == OptionState.enBGMParamator)
+        {
+            m_saveDataManager.SaveData.saveData.BGMVolume = (float)m_soundDatas[number].soundStage / SoundStage;
+            m_bgm.ResetVolume();
+        }
+        SE_CursorMove.PlaySE();
+        m_saveDataManager.Save();
+    }
+
+    /// <summary>
+    /// 音量を下げる処理。
+    /// </summary>
+    private void VolumeDown(int number)
+    {
+        m_soundDatas[number].soundStage--;
+        if (m_soundDatas[number].soundStage < 0)
+        {
+            m_soundDatas[number].soundStage = 0;
+            SE_Error.PlaySE();
+            return;
+        }
+
+        MoveX(m_soundDatas[number].rectTransform, m_moveLength);
+        SoundText[number].GetComponent<TextMeshProUGUI>().text = m_soundDatas[number].soundStage.ToString();
+
+        if (m_comandState == OptionState.enSEParamator)
+        {
+            m_saveDataManager.SaveData.saveData.SEVolume = (float)m_soundDatas[number].soundStage / SoundStage;
+        }
+        if (m_comandState == OptionState.enBGMParamator)
+        {
+            m_saveDataManager.SaveData.saveData.BGMVolume = (float)m_soundDatas[number].soundStage / SoundStage;
+            m_bgm.ResetVolume();
+        }
+        SE_CursorMove.PlaySE();
+        m_saveDataManager.Save();
+    }
+
+    /// <summary>
+    /// 移動処理。
+    /// </summary>
+    /// <param name="MoveLength">移動量。</param>
+    /// <param name="rectTransform">対象オブジェクトの座標。</param>
+    private void MoveX(RectTransform rectTransform, float MoveLength)
+    {
+        var length = rectTransform.anchoredPosition.x + MoveLength;
+        var x = Mathf.Clamp(length, PositionX_Min, PositionX_Max);
+        rectTransform.anchoredPosition = new Vector2(x, rectTransform.anchoredPosition.y);
+        Debug.Log(rectTransform.anchoredPosition);
+    }
+
+
+    /// <summary>
+    /// カメラのパラメータを設定する処理。
+    /// </summary>
+    private void SetCameraParamator()
+    {
+        if (m_comandState != OptionState.enCameraParamator)
+        {
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            PushRight();
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            PushLeft();
+        }
+
+        // ゲームパッドが接続されていない場合。
+        if(m_gamepad == null)
+        {
+            return;
+        }
+
+        if (m_gamepad.dpad.right.wasPressedThisFrame)
+        {
+            PushRight();
+        }
+        if (m_gamepad.dpad.left.wasPressedThisFrame)
+        {
+            PushLeft();
+        }
+    }
+
+    /// <summary>
+    /// →キーを押したときの処理。
+    /// </summary>
+    private void PushRight()
+    {
+        if (m_saveDataManager.SaveData.saveData.CameraStete == false)
+        {
+            SE_Error.PlaySE();
+            return;
+        }
+        ChangeCameraRotation(false);
+        SE_CursorMove.PlaySE();
+        m_saveDataManager.Save();
+    }
+
+    /// <summary>
+    /// ←キーを押したときの処理。
+    /// </summary>
+    private void PushLeft()
+    {
+        if (m_saveDataManager.SaveData.saveData.CameraStete == true)
+        {
+            SE_Error.PlaySE();
+            return;
+        }
+        ChangeCameraRotation(true);
+        SE_CursorMove.PlaySE();
+        m_saveDataManager.Save();
+    }
+
+    /// <summary>
+    /// カメラ回転を変更する。
+    /// </summary>
+    /// <param name="flag">falseならノーマル回転。tureならリバース回転。</param>
+    private void ChangeCameraRotation(bool flag)
+    {
+        var text = "ノーマル";
+        int notActivenumber = 0;
+        int activeNumber = 1;
+        if(flag == true)
+        {
+            text = "リバース";
+            notActivenumber = 1;
+            activeNumber = 0;
+        }
+
+        CameraCursor[notActivenumber].SetActive(false);
+        CameraCursor[activeNumber].SetActive(true);
+        CameraText.GetComponent<TextMeshProUGUI>().text = $"{text}";
+        m_saveDataManager.SaveData.saveData.CameraStete = flag;
+    }
+
+    /// <summary>
+    /// データを初期化する。
+    /// </summary>
+    public void ResetStatus()
+    {
+        m_saveDataManager.InitOption();
+        Init();
+        m_saveDataManager.Save();
+    }
+}
