ef8f6fed2069273cdcfd4a64de8d8cf4571ee61d
maekawa <CA01972033@st.kawahara.ac.jp>
2024-11-05 11:14:40 +0900

    変数名を修正

--- Assets/Scripts/StageSelect/OptionScene.cs
+++ Assets/Scripts/StageSelect/OptionScene.cs
@@ -17,14 +17,14 @@ public class OptionsMenu : MonoBehaviour
     public TextMeshProUGUI bgmVolumeText;  //BGMのボリュームを表示するテキスト
     public TextMeshProUGUI seVolumeText;   //SEのボリュームを表示するテキスト
 
-    private float bgmValue;
-    private float seValue;
+    private float m_bgmValue;
+    private float m_seValue;
 
-    private int selectedIndex = 0;  //選択中のインデックス
+    private int m_selectedIndex = 0;  //選択中のインデックス
     private GameObject[] menuItems;
 
-    private int cameraIndex = 0;    //カメラ設定のインデックス
-    private string[] cameraOptionName = { "ノーマル", "たけのこ" };
+    private int m_cameraIndex = 0;    //カメラ設定のインデックス
+    private string[] cameraOptionName = { "ノーマル", "たけのこ  " };
 
     [SerializeField,Header("スライダー選択時のポジション調整")]
     private Vector3 SliderAdjustmentPosition;   //スライダー選択時のカーソルのポジションの調整に使う
@@ -71,12 +71,12 @@ public class OptionsMenu : MonoBehaviour
         {
             // インデックスを変更（上下移動）
             int direction = -1;
-            selectedIndex = Mathf.Clamp(selectedIndex + direction, 0, menuItems.Length - 1);           
+            m_selectedIndex = Mathf.Clamp(m_selectedIndex + direction, 0, menuItems.Length - 1);           
         }
         else if(Gamepad.current.dpad.down.wasPressedThisFrame)
         {
             int direction = 1;
-            selectedIndex = Mathf.Clamp(selectedIndex + direction, 0, menuItems.Length - 1);
+            m_selectedIndex = Mathf.Clamp(m_selectedIndex + direction, 0, menuItems.Length - 1);
         }
     }
 
@@ -88,17 +88,17 @@ public class OptionsMenu : MonoBehaviour
         for(int i = 0;i < menuItems.Length;i++)
         {
             //スライダーが選択されているとき
-            if(i == selectedIndex && menuItems[selectedIndex].TryGetComponent(out Slider selectedSlider))
+            if(i == m_selectedIndex && menuItems[m_selectedIndex].TryGetComponent(out Slider selectedSlider))
             {
                 selectCursor.transform.position = menuItems[i].transform.position + SliderAdjustmentPosition;
             }
             //カメラ設定が選択されているとき
-            if (i == selectedIndex && menuItems[selectedIndex].TryGetComponent(out TextMeshProUGUI selectedText))
+            if (i == m_selectedIndex && menuItems[m_selectedIndex].TryGetComponent(out TextMeshProUGUI selectedText))
             {
                 selectCursor.transform.position = menuItems[i].transform.position + CameraAdjustmentPosition;
             }
             //戻るボタンが選択されているとき
-            if (i == selectedIndex && menuItems[selectedIndex].TryGetComponent(out Image selectedImage))
+            if (i == m_selectedIndex && menuItems[m_selectedIndex].TryGetComponent(out Image selectedImage))
             {
                 selectCursor.transform.position = menuItems[i].transform.position + ImageAdjustmentPosition;
             }
@@ -111,7 +111,7 @@ public class OptionsMenu : MonoBehaviour
     void ChangeSliderValue()
     {
         // 選択項目がスライダーなら左右入力で値を調整
-        if (menuItems[selectedIndex].TryGetComponent(out Slider selectedSlider))
+        if (menuItems[m_selectedIndex].TryGetComponent(out Slider selectedSlider))
         {
             if (Gamepad.current.dpad.right.wasPressedThisFrame)
                 selectedSlider.value += 0.1f;
@@ -126,12 +126,12 @@ public class OptionsMenu : MonoBehaviour
     void ChangeVolumeText()
     {
         //スライダーでおこる誤差を四捨五入して調整
-        bgmValue = Mathf.Round(bgmSlider.value * 10f) / 10f;
-        seValue = Mathf.Round(seSlider.value * 10f) / 10f;
+        m_bgmValue = Mathf.Round(bgmSlider.value * 10f) / 10f;
+        m_seValue = Mathf.Round(seSlider.value * 10f) / 10f;
 
         //bgmとseの値をテキストに代入
-        bgmVolumeText.text = bgmValue.ToString();
-        seVolumeText.text = seValue.ToString();
+        bgmVolumeText.text = m_bgmValue.ToString();
+        seVolumeText.text = m_seValue.ToString();
 
     }
 
@@ -141,18 +141,18 @@ public class OptionsMenu : MonoBehaviour
     void ChangeCameraOption()
     {
         //選択項目がカメラ設定だったら左右入力で設定変更
-        if(menuItems[selectedIndex] == cameraOption.gameObject)
+        if(menuItems[m_selectedIndex] == cameraOption.gameObject)
         {
             if(Gamepad.current.dpad.right.wasPressedThisFrame)
             {
-                cameraIndex = (cameraIndex + 1 + cameraOptionName.Length) % cameraOptionName.Length;
+                m_cameraIndex = (m_cameraIndex + 1 + cameraOptionName.Length) % cameraOptionName.Length;
             }
             else if (Gamepad.current.dpad.left.wasPressedThisFrame)
             {
-                cameraIndex = (cameraIndex - 1 + cameraOptionName.Length) % cameraOptionName.Length;
+                m_cameraIndex = (m_cameraIndex - 1 + cameraOptionName.Length) % cameraOptionName.Length;
             }
         }
-        cameraOption.text = cameraOptionName[cameraIndex];
+        cameraOption.text = cameraOptionName[m_cameraIndex];
     }
 
     /// <summary>
@@ -161,7 +161,7 @@ public class OptionsMenu : MonoBehaviour
     void BackToStageSelect()
     {
         // 「戻る」ボタンが選択されていてBボタンを押すかStartボタンを押すと戻る処理を実行
-        if (menuItems[selectedIndex].TryGetComponent(out Image selectedImage) && Gamepad.current.bButton.wasPressedThisFrame || Gamepad.current.startButton.wasPressedThisFrame)
+        if (menuItems[m_selectedIndex].TryGetComponent(out Image selectedImage) && Gamepad.current.bButton.wasPressedThisFrame || Gamepad.current.startButton.wasPressedThisFrame)
         {
             Stage.SetActive(true);
             Option.SetActive(false);
