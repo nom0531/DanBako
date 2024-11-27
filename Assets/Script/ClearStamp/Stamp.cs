dee3012468207c266146e23b6a5e8025d3bcebd4
nom0531 <norrrrrra0504@gmail.com>
2024-11-27 13:35:34 +0900

    クリアスタンプを実装。

--- Assets/Script/ClearStamp/Stamp.cs
+++ Assets/Script/ClearStamp/Stamp.cs
@@ -4,22 +4,19 @@ using UnityEngine;
 
 public class Stamp : MonoBehaviour
 {
-    private SaveDataManager m_saveDataManager;
     private Animator m_animator;
 
     // Start is called before the first frame update
     void Start()
     {
-        m_saveDataManager = GameManager.Instance.SaveDataManager;
         m_animator = GetComponent<Animator>();
-
     }
 
     /// <summary>
     /// アニメーションを再生する。
     /// </summary>
-    private void PlayAnimation()
+    public void PlayAnimation()
     {
-
+        m_animator.SetTrigger("Active");
     }
 }
