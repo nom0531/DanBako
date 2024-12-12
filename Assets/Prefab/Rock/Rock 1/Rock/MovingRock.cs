cd79b2c94498b5542ae0fb70f77d4cde9e23d8db
CA01972018 <1981115290@qq.com>
2024-12-12 20:40:29 +0900

    石のプレハブ

---
+++ Assets/Prefab/Rock/Rock 1/Rock/MovingRock.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using UnityEngine;
+
+public class MovingRock : MonoBehaviour
+{
+    [SerializeField] Animator m_RockAnimator;
+    private bool isRewind = false;
+
+    void Update()
+    {
+        // Zキーが押されたらアニメーションを再生
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            PlayAnimation();
+        }
+
+        // Xキーが押されたら巻き戻しを実行
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            StartCoroutine(TriggerRewindWithDelay());
+        }
+    }
+
+    void PlayAnimation()
+    {
+        isRewind = false; // 巻き戻しフラグをリセット
+        m_RockAnimator.SetBool("IsRewind", false); // 巻き戻しフラグをオフ
+        m_RockAnimator.SetBool("IsPlaying", true); // 再生フラグをオン
+    }
+
+    IEnumerator TriggerRewindWithDelay()
+    {
+        // トリガーを設定
+        m_RockAnimator.SetTrigger("Rewind");
+
+        // 2秒待機
+        yield return new WaitForSeconds(1.0f);
+
+        // フラグを変更して巻き戻し開始
+        TriggerRewind();
+    }
+
+    void TriggerRewind()
+    {
+        isRewind = true; // 巻き戻しフラグを設定
+        m_RockAnimator.SetBool("IsPlaying", false); // 再生フラグをオフ
+        m_RockAnimator.SetBool("IsRewind", true); // 巻き戻しフラグをオン
+    }
+}
